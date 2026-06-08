ec0b981410e3879e3f53a4b84a1dac990e16fca8
ashishsingh200 <ashishsinghtkg@gmail.com>
2026-06-08 13:17:07 +0530

    modified

--- 03. Streamlit/app.py
+++ 03. Streamlit/app.py
@@ -3,3 +3,8 @@ st.write('Hello World')
 
 st.title("Hello Streamlit")
 st.write("Ashish Kumar Singh")
+
+st.header("welcome to Streamlit")
+
+st.subheader("This is a Sub-header")
+st.text("THis is a plain text")
