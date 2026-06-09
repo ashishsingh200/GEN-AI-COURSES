9d7994c282d9d9a291f7510463a8f297e5a050d1
ashishsingh200 <ashishsinghtkg@gmail.com>
2026-06-09 06:32:03 +0530

    Completed assignment 8

--- Assignments/GEN AI Assignment-8(Ashish)/app_dashboard.py
+++ Assignments/GEN AI Assignment-8(Ashish)/app_dashboard.py
@@ -2,4 +2,24 @@ import streamlit as st
 
 st.title("Simple Sales Dashboard")
 
-st.selectbox
+selected_month=st.selectbox("Months",
+             ['January','February','March','April'],
+             placeholder="Select a month",
+             index=None
+             )
+
+sales={
+    'January':1200,
+    'February':1500,
+    'March': 900,
+    'April':2000
+}
+
+if selected_month:
+    current_sales=sales[selected_month]
+    st.metric(label=f"Sales for {selected_month}", value=f"${current_sales: ,}")
+
+st.divider()
+
+st.subheader("Monthly sales Overview")
+st.bar_chart(list(sales.values()))
